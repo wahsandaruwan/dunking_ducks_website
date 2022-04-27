77e5219b1f9455dc280688e22ed9ddfe3598fbfe
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-04-27 22:58:50 +0530

    Nav bar done

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return (
+        <>
+            <nav className="nav-bar">
+                <div className="logo">
+                    Crypto<span>Hub</span>
+                </div>
+                <ul className="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
