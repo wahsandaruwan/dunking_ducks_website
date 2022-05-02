bd47050949828b37be2cc20932c64e1da96c0fd2
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-05-03 02:02:26 +0530

    Faq dropdown icon changed

--- src/Components/QnA.js
+++ src/Components/QnA.js
@@ -1,12 +1,12 @@
+import { IoIosArrowDropdown } from "react-icons/io"
+
 const QnA = ({ question, answer, clickFunc, clickedState, indexValue }) => {
     return (
         <>
             <div className="qna-content" onClick={() => clickFunc(indexValue)}>
                 <div className="question">
                     <p className="question-txt">{question}</p>
-                    <svg className={`dropdown-icon ${clickedState === indexValue ? "rotate" : ""}`} width="15" height="10" viewBox="0 0 42 25">
-                        <path d="M3 3L21 21L39 3" stroke="#2c3e50" stroke-width="7" stroke-linecap="round" fill="none" />
-                    </svg>
+                    <IoIosArrowDropdown className={`dropdown-icon ${clickedState === indexValue ? "rotate" : ""}`} />
                 </div>
                 <div div className={`answer ${clickedState === indexValue ? "show" : ""}`}>
                     <p className="answer-txt">{answer}</p>
